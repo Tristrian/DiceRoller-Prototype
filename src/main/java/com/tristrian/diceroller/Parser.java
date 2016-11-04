9f524dccd265f79b81e2ee06e5b75045468067c8
Tristrian <Tiago.A.C.9@hotmail.com>
2016-11-04 16:06:31 +0100

    Update Parser.java

--- src/main/java/com/tristrian/diceroller/Parser.java
+++ src/main/java/com/tristrian/diceroller/Parser.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tristrian.diceroller;
 
 import java.util.ArrayList;
@@ -12,10 +7,6 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-/**
- *
- * @author AlmeidaCorreiaT
- */
 public class Parser {
 
     private final Roller roller;
@@ -34,7 +25,6 @@ public class Parser {
     public ArrayList<Integer> parseCommand(String string) {
         result = new ArrayList<>();
         String[] argument = string.split(" ");
-        //On vérifie s'il s'agit de la argument dndroll
         for (String argument1 : argument) {
             diceResult = new ArrayList<>();
             dicesArg = new ArrayList<>();
